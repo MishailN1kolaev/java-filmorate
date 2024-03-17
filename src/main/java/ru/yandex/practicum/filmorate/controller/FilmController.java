6cb70d6e27ff598e626a8a6e5e023f540dd9fbf8
Mishail Nikolaev <mishail.nikolaev2017@yandex.ru>
2024-03-17 23:42:48 +0300

    ТЗ 10 исправил ошибки

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -3,11 +3,10 @@ package ru.yandex.practicum.filmorate.controller;
 import java.util.List;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
-import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+
 
 @RestController
 @RequestMapping("/films")
