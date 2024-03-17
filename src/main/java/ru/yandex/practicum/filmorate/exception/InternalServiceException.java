f8c00894dc7db445aa564501066b31b73f1560e8
Mishail Nikolaev <mishail.nikolaev2017@yandex.ru>
2024-03-17 17:16:23 +0300

    ТЗ 10 попытка 3

--- src/main/java/ru/yandex/practicum/filmorate/exception/InternalServiceException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/InternalServiceException.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
     public class InternalServiceException extends RuntimeException {
 
         public InternalServiceException(final String message) {
+            super(message);
             log.error(message);
         }
     }
