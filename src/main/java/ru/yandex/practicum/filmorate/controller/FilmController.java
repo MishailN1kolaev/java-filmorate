98bfbd8aca53b258e0acc25d4d32959e779382dd
Mishail Nikolaev <mishail.nikolaev2017@yandex.ru>
2024-04-07 14:58:29 +0300

    10 ТЗ

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,51 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
+
 
 @RestController
+@RequestMapping("/films")
+@RequiredArgsConstructor
 public class FilmController {
+    private final FilmService filmService;
+
+    @PostMapping
+    public Film createFilm(@Valid @RequestBody Film film) {
+        return filmService.createFilm(film);
+    }
+
+    @GetMapping
+    public List<Film> getFilms() {
+        return filmService.getFilms();
+    }
+
+    @PutMapping
+    public Film updateFilm(@Valid @RequestBody Film film) {
+        return filmService.updateFilm(film);
+    }
+
+    @GetMapping("/{id}")
+    public Film getFilmById(@PathVariable Long id) {
+        return filmService.getFilmById(id);
+    }
+
+    @GetMapping("/popular")
+    public List<Film> getPopularMovies(@RequestParam(defaultValue = "10") Integer count) {
+        return filmService.getPopularMovies(count);
+    }
+
+    @PutMapping("/{id}/like/{userId}")
+    public void likeAMovie(@PathVariable Long id, @PathVariable Long userId) {
+        filmService.like(id, userId);
+    }
+
+    @DeleteMapping("/{id}/like/{userId}")
+    public void removeLike(@PathVariable Long id, @PathVariable Long userId) {
+        filmService.dislike(id, userId);
+    }
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -0,0 +1,19 @@
+package ru.yandex.practicum.filmorate.storage.film;
+
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.List;
+
+public interface FilmStorage {
+
+    Film createFilm(Film film);
+
+    Film updateFilm(Film film);
+
+    void deleteFilms();
+
+    Film getFilmById(Long id);
+
+    List<Film> getFilms();
+
+}
